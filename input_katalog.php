c6944041a5defae6edac35d10b8b62ad4538f2e3
Natalieefd <93095344+Natalieefd@users.noreply.github.com>
2022-10-20 23:18:09 +0800

    Add files via upload

---
+++ input_katalog.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="input_katalog.css">
+    <title>Input Katalog | Cozy Furniture</title>
+</head>
+<body>
+    <div class="header-brand">
+        <div class="first_name">cozy</div>
+        <div class="last_name">furniture</div>
+    </div>
+    <div><i class="fa-solid fa-chevron-left fa-2xl"></i></div>
+    <form action="input_katalog.php" method="post"></form>
+    <div class="container">
+        <div class="content">
+            <div class="input-box">
+                <div class="header">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="footer-c">
+        Copyright &copy; 2022 Designed by Natalie Fuad
+    </div>
+</body>
+</html>
